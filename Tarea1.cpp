e7048bc92b8e91ed6ff46cb049faf6f05901dc89
Juan Castelli <juancastelli12345@gmail.com>
2019-03-28 19:49:15 -0300

    Commit tarea 3

--- Tarea1.cpp
+++ Tarea1.cpp
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
-enum TRaza{Orco, Humano, Mago, Enano, Elfo}
-char Nombres[6][15]={"Merodeador", "Rompehuesos", "Algoritmico", "Proscripto", "Cruzamontañas"}
-char Apellidos[6][10]={"Furioso", "Mistico", "Peregrino", "Galactico", "Sabio"}
+enum TRaza{Orco, Humano, Mago, Enano, Elfo};
+char Nombres[6][15]={"Merodeador ", "Rompehuesos ", "Algoritmico ", "Proscripto ", "Cruzamontañas "};
+char Apellidos[6][10]={"Furioso ", "Mistico ", "Peregrino ", "Galactico ", "Sabio "};
 
 struct TDatos {
 	TRaza Raza; //nota 1
@@ -20,14 +21,34 @@ struct TCaracteristicas{
 	int Armadura; //1 a 10
 };
 struct TPersonaje {
-	TDatos * DatosPersonales
-	TCaracteristicas * Caracteristicas
+	TDatos * DatosPersonales;
+	TCaracteristicas * Caracteristicas;
 };
 void cargarcaract(struct TCaracteristicas * CaracPers);
 void mostrarcaract(struct TCaracteristicas * CaracPers);
 void cargar_datos(struct TDatos *puntero);
 void mostrar_datos(struct TDatos *a);
+
 int main(void){
+	srand(time(NULL));
+	struct TPersonaje * Lista;
+	int cant;
+	printf("Cuantos personajes desea cargar?:\n");
+	scanf("%d", &cant);
+	Lista = (struct TPersonaje*) malloc(sizeof(struct TPersonaje)*cant);
+	for (int i = 0; i < cant; ++i)
+	{
+		fflush(stdin);
+		cargar_datos(Lista[i].DatosPersonales);
+		cargarcaract(Lista[i].Caracteristicas);
+		printf("\n");
+	}
+	for (int i = 0; i < cant; ++i)
+	{
+		printf("Personaje %d:\n", (i+1));
+		mostrar_datos(Lista[i].DatosPersonales);
+		mostrarcaract(Lista[i].Caracteristicas);
+	}
 	return 0;
 }
 
@@ -37,6 +58,7 @@ void cargarcaract(struct TCaracteristicas * CaracPers){
 	CaracPers->fuerza = rand()%10 + 1;
 	CaracPers->Nivel = rand()%10 + 1;
 	CaracPers->Armadura = rand()%10 + 1;
+	printf("Hola\n");
 }
 void mostrarcaract(struct TCaracteristicas * CaracPers){
 	printf("Velocidad: %d\n", CaracPers->velocidad);
@@ -50,13 +72,30 @@ void mostrarcaract(struct TCaracteristicas * CaracPers){
 
 
 void cargar_datos(struct TDatos *puntero){
-	char AM[20];
+	char AM[30];
 	strcpy(AM, Apellidos[rand()%7]);
 	strcat(AM, Nombres[rand()%7]);
-	ApellidoNombre = (char*) malloc(sizeof(char) * strlenght(AM));
-	strcpy(puntero->Raza, Raza[rand()%6]);
+	puntero->ApellidoNombre = (char*) malloc(sizeof(char) * strlen(AM));
+	switch(rand()%5){
+		case 0:
+		puntero->Raza = Orco;
+		break;
+		case 1:
+		puntero->Raza = Humano;
+		break;
+		case 2:
+		puntero->Raza = Mago;
+		break;
+		case 3:
+		puntero->Raza = Enano;
+		break;
+		case 4:
+		puntero->Raza = Elfo;
+		break;
+	}
 	strcpy(puntero->ApellidoNombre, AM);
-	puntero->edad = rand()%300;
+	puts(puntero->ApellidoNombre);
+	puntero->edad = rand()%301;
 	puntero->Salud = 100;
 
 }
@@ -65,8 +104,7 @@ void cargar_datos(struct TDatos *puntero){
 
 void mostrar_datos(struct TDatos *a){
 	
-	printf("\n Raza: ", );
-	puts(a->Raza);
+	printf("\n Raza: %d", a->Raza);
 	printf("\n Apellido y Nombre: ");
 	puts(a->ApellidoNombre);
 	printf("\n Edad: %d", a->edad);
