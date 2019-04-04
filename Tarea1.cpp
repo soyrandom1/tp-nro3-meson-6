8eb8aa2c8c914106b19b992da6183868bb481bad
Juan Castelli <juancastelli12345@gmail.com>
2019-04-04 18:53:09 -0300

    Arreglo de personajes listo

--- Tarea1.cpp
+++ Tarea1.cpp
@@ -2,10 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <conio.h>
 
 enum TRaza{Orco, Humano, Mago, Enano, Elfo};
-char Nombres[6][15]={"Merodeador ", "Rompehuesos ", "Algoritmico ", "Proscripto ", "Cruzamontañas "};
-char Apellidos[6][10]={"Furioso ", "Mistico ", "Peregrino ", "Galactico ", "Sabio "};
+char Nombres[5][20]={"Merodeador", "Rompehuesos", "Algoritmico", "Proscripto", "Cruzamontanias"};
+char Apellidos[5][20]={" Furioso", " Mistico", " Peregrino", " Galactico", " Sabio"};
 
 struct TDatos {
 	TRaza Raza; //nota 1
@@ -21,8 +22,8 @@ struct TCaracteristicas{
 	int Armadura; //1 a 10
 };
 struct TPersonaje {
-	TDatos * DatosPersonales;
-	TCaracteristicas * Caracteristicas;
+	TDatos * DatosPersonales;  // null
+	TCaracteristicas * Caracteristicas; // null 
 };
 void cargarcaract(struct TCaracteristicas * CaracPers);
 void mostrarcaract(struct TCaracteristicas * CaracPers);
@@ -35,19 +36,20 @@ int main(void){
 	int cant;
 	printf("Cuantos personajes desea cargar?:\n");
 	scanf("%d", &cant);
-	Lista = (struct TPersonaje*) malloc(sizeof(struct TPersonaje)*cant);
-	for (int i = 0; i < cant; ++i)
+	Lista = (struct TPersonaje *) malloc(sizeof(struct TPersonaje)*cant);
+	for (int t = 0; t < cant; ++t)
 	{
 		fflush(stdin);
-		cargar_datos(Lista[i].DatosPersonales);
-		cargarcaract(Lista[i].Caracteristicas);
-		printf("adsfsdgas\n");
+		Lista[t].DatosPersonales = (struct TDatos * )malloc(sizeof(struct TDatos));
+		Lista[t].Caracteristicas = (struct TCaracteristicas * )malloc(sizeof(struct TCaracteristicas));
+		cargar_datos(Lista[t].DatosPersonales); // (personaje . datospersonales) (* Tdatos)
+		cargarcaract(Lista[t].Caracteristicas);
 	}
-	for (int i = 0; i < cant; ++i)
+	for (int k = 0; k < cant; ++k)
 	{
-		printf("Personaje %d:\n", (i+1));
-		mostrar_datos(Lista[i].DatosPersonales);
-		mostrarcaract(Lista[i].Caracteristicas);
+		printf("\nPersonaje %d:\n", (k+1));
+		mostrar_datos(Lista[k].DatosPersonales);
+		mostrarcaract(Lista[k].Caracteristicas);
 	}
 	return 0;
 }
@@ -58,8 +60,8 @@ void cargarcaract(struct TCaracteristicas * CaracPers){
 	CaracPers->fuerza = rand()%10 + 1;
 	CaracPers->Nivel = rand()%10 + 1;
 	CaracPers->Armadura = rand()%10 + 1;
-	printf("Hola\n");
 }
+
 void mostrarcaract(struct TCaracteristicas * CaracPers){
 	printf("Velocidad: %d\n", CaracPers->velocidad);
 	printf("Destreza: %d\n", CaracPers->destreza);
@@ -68,46 +70,60 @@ void mostrarcaract(struct TCaracteristicas * CaracPers){
 	printf("Armadura: %d\n", CaracPers->Armadura);
 }
 
-
-
-
 void cargar_datos(struct TDatos *puntero){
-	char AM[30];
-	strcpy(AM, Apellidos[rand()%7]);
-	strcat(AM, Nombres[rand()%7]);
-	//puntero->ApellidoNombre = (char*) malloc(sizeof(char) * strlen(AM));
+	char AM[100];
+
+	strcpy(AM, Nombres[rand()%5]);
+	strcat(AM, Apellidos[rand()%5]);
+	puntero->ApellidoNombre = (char*) malloc(sizeof(char) * strlen(AM) + 2);
+
 	switch(rand()%5){
 		case 0:
-		puntero->Raza = Orco;
-		break;
+			puntero->Raza = Orco;
+			break;
 		case 1:
-		puntero->Raza = Humano;
-		break;
+			puntero->Raza = Humano;
+			break;
 		case 2:
-		puntero->Raza = Mago;
-		break;
+			puntero->Raza = Mago;
+			break;
 		case 3:
-		puntero->Raza = Enano;
-		break;
+			puntero->Raza = Enano;
+			break;
 		case 4:
-		puntero->Raza = Elfo;
-		break;
+			puntero->Raza = Elfo;
+			break;
 	}
 	strcpy(puntero->ApellidoNombre, AM);
-	puts(puntero->ApellidoNombre);
 	puntero->edad = rand()%301;
 	puntero->Salud = 100;
+	printf("\n");
 
 }
 
-
-
 void mostrar_datos(struct TDatos *a){
 	
-	printf("\n Raza: %d", a->Raza);
-	printf("\n Apellido y Nombre: ");
-	puts(a->ApellidoNombre);
-	printf("\n Edad: %d", a->edad);
-	printf("\n Salud: %.2lf", a->Salud);
+	printf("\nRaza:");
+	switch(a->Raza){
+		case 0:
+			printf("Orco\n");
+			break;
+		case 1:
+			printf("Humano\n");
+			break;
+		case 2:
+			printf("Mago\n");
+			break;
+		case 3:
+			printf("Enano\n");
+			break;
+		case 4:
+			printf("Elfo\n");
+			break;
+	}
+	printf("Apellido y Nombre: %s \n", a->ApellidoNombre);
+	printf("Edad: %d", a->edad);
+	printf("\nSalud: %.2lf", a->Salud);
+	printf("\n");
 
 }
